f78f553a13f43a6d378bcdcbbdeade08abbfa907
RAJAT0444 <rajatkumar6072@gmail.com>
2025-06-03 14:09:22 +0530

    trigger redeploy

--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -1,21 +1,26 @@
-// import {z} from 'zod';
 
-// export const signInSchema = z.object({ 
-//     email: z.string()
-//         .email('Invalid email address')
-//         .max(50, 'Email must be at most 50 characters long'),
-//     password: z.string()
-//         .min(8, 'Password must be at least 8 characters long')
+
+
+
+// import { z } from "zod";
+
+// export const signInSchema = z.object({
+//   email: z
+//     .string()
+//     .email("Invalid email address")
+//     .max(50, "Email must be at most 50 characters long"),
+//   password: z.string().min(8, "Password must be at least 8 characters long"),
 // });
 
 
 
-import { z } from "zod";
+
+
+import * as z from 'zod';
 
 export const signInSchema = z.object({
-  email: z
-    .string()
-    .email("Invalid email address")
-    .max(50, "Email must be at most 50 characters long"),
-  password: z.string().min(8, "Password must be at least 8 characters long"),
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
 });
+
+
