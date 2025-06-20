8dc34f79f741548ed2ccfafa0b50340cdbc2e50c
RAJAT KUMAR <134751273+RAJAT0444@users.noreply.github.com>
2025-06-20 20:03:48 +0530

    Update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,7 +7,7 @@ interface RootLayoutProps {
   children: React.ReactNode;
 }
 
-
+// if you need to import Navbaar into dashboard to add but security purpose dont import 
 
 
 export default function DashboardLayout({ children }: RootLayoutProps) {
