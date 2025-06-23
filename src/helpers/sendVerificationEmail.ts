de3a235709221d21cc940e2ec1848439277c6b20
RAJAT KUMAR <134751273+RAJAT0444@users.noreply.github.com>
2025-06-23 13:08:20 +0530

    Update sendVerificationEmail.ts

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,13 +1,12 @@
 import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
 import { ApiResponse } from "@/types/ApiResponse";
-import { Message } from "@/model/User";
 
 export async function sendVerificationEmail(
   email: string,
   username: string,
   verifyCode: string
-): Promise<ApiResponse<Message>> {
+): Promise<ApiResponse<void>> {
   try {
     await resend.emails.send({
       from: "verify@conreach.in", // ✅ Must match verified sender in Resend dashboard
@@ -24,7 +23,7 @@ export async function sendVerificationEmail(
       message: "Verification email sent successfully",
     };
   } catch (emailError) {
-    console.error("Error sending verification email:", emailError); // ✅ Add this for debugging
+    console.error("Error sending verification email:", emailError);
     return {
       success: false,
       message: "Failed to send verification email",
