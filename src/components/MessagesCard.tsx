1f6d4c360a017f9407c87631caabadc3a2b39057
RAJAT KUMAR <134751273+RAJAT0444@users.noreply.github.com>
2025-06-23 13:02:35 +0530

    Update MessagesCard.tsx

--- src/components/MessagesCard.tsx
+++ src/components/MessagesCard.tsx
@@ -98,14 +98,13 @@
 
 
 
-
 'use client';
 
 import React from 'react';
 import axios, { AxiosError } from 'axios';
 import dayjs from 'dayjs';
 import { X } from 'lucide-react';
-import { Message } from '@/model/User';
+import { IMessage } from '@/model/User';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import {
   AlertDialog,
@@ -123,7 +122,7 @@ import { toast } from 'sonner';
 import { ApiResponse } from '@/types/ApiResponse';
 
 type MessageCardProps = {
-  message: Message;
+  message: IMessage;
   onMessageDelete: (messageId: string) => void;
 };
 
@@ -159,8 +158,7 @@ export function MessageCard({ message, onMessageDelete }: MessageCardProps) {
               <AlertDialogHeader>
                 <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
                 <AlertDialogDescription>
-                  This action cannot be undone. This will permanently delete
-                  this message.
+                  This action cannot be undone. This will permanently delete this message.
                 </AlertDialogDescription>
               </AlertDialogHeader>
               <AlertDialogFooter>
